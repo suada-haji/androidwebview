00c6cabc0ad8315c58f37bd3550f47ca240e4873
Suada Haji <suada.haji@andela.com>
2017-04-05 15:23:37 +0300

    Add assets and modify MainActivity

--- app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
+++ app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.suadahaji.webviewapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.webView)
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView.loadUrl("http://google.com");
 
     }
 }
