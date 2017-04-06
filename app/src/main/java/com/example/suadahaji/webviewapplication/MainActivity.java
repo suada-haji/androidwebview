5536dafae4e99f33c29f0b4789937f7fa4215b1c
Suada Haji <suada.haji@andela.com>
2017-04-06 14:48:44 +0300

    Modify MainActivity

--- app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
+++ app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.suadahaji.webviewapplication;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import butterknife.BindView;
 
@@ -11,12 +19,64 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.webView)
     WebView webView;
 
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+
+    @BindView(R.id.backdrop)
+    ImageView imageView;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private String postUrl= "http://api.androidhive.info/webview/index.html";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        webView.loadUrl("http://google.com");
+        initCollapsingToolbar();
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(postUrl);
+        webView.setHorizontalScrollBarEnabled(false);
+    }
+
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the txtPostTitle when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle("Web View");
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+
+            }
+        });
+
+        // loading toolbar header image
+        Glide.with(getApplicationContext()).load("http://api.androidhive.info/webview/nougat.jpg")
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
 
     }
 }
