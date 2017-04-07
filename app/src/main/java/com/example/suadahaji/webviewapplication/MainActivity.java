d2c933c2662d9a3dcaf182e1a00cc06f4c59afc3
Suada Haji <suada.haji@andela.com>
2017-04-07 21:32:10 +0300

    Add webview feature

--- app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
+++ app/src/main/java/com/example/suadahaji/webviewapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.suadahaji.webviewapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -13,38 +17,52 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.webView)
+    @BindView(R.id.myWebView)
     WebView webView;
 
-    @BindView(R.id.progressBar)
-    ProgressBar progressBar;
 
-    @BindView(R.id.backdrop)
-    ImageView imageView;
-
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
-    private String postUrl= "http://api.androidhive.info/webview/index.html";
+    private String postUrl= "http://www.centerend.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar(toolbar);
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        initCollapsingToolbar();
+        ButterKnife.bind(this);
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(postUrl);
-        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new MyWebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
-    private void initCollapsingToolbar() {
+    // Use When the user clicks a link from a web page in your WebView
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            if (Uri.parse(url).getHost().equals("www.centerend.com")) {
+
+                return false;
+
+            }
+
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+            startActivity(intent);
+
+            return true;
+        }
+    }
+
+   /* private void initCollapsingToolbar() {
         final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(" ");
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
@@ -78,5 +96,5 @@ public class MainActivity extends AppCompatActivity {
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(imageView);
 
-    }
+    }*/
 }
